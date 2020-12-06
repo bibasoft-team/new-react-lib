ab5f83a1f6f9563158add09d1630f11bb3b442a9
Stepan Kotov <rediska1114@gmail.com>
2020-12-06 19:37:21 +0300

    update template and add cli

---
+++ README.md
@@ -0,0 +1,18 @@
+# Utility for creating React libraries
+
+## use:
+
+```
+npm i -g new-react-lib
+new-react-lib my-awesome-lib
+```
+
+or
+
+```
+npx new-react-lib my-awesome-lib
+```
+
+## Features
+
+- different templates (custom templates in future)
--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 #!/usr/bin/env node
 const { program } = require('commander');
+const shelljs_exec = require('shelljs.exec')
 const packageJson = require('../package.json')
+const fs = require('fs-extra')
+const chalk = require('chalk');
+
+const exec = (commands) => {
+    return shelljs_exec(Array.isArray(commands) ? commands.join(' ') : commands, { stdio: 'inherit' })
+}
+
+const error = chalk.bold.red;
+const warning = chalk.keyword('orange');
+
 
 program
     .version(packageJson.version)
@@ -10,9 +21,24 @@ program
         name: 'name of library',
     })
     .action(function (name) {
-        console.log('command:', name);
+        const template = program.template
+        if (!fs.pathExistsSync(`templates/${template}`)) {
+            error(`Error: template ${name} does not exists!`)
+            return
+        }
+        if (fs.pathExistsSync(name)) {
+            error(`Error: directory ${name} already exists!`)
+            return
+        }
+        exec(`mkdir ${name}`)
+        chalk('copy template...')
+        fs.copySync(`templates/${typescript}`, name)
+        chalk('install dependencies...')
+        // exec('npm i')
+        chalk.green(`Complete!`)
+        chalk.blue(`Library ${name} created successfully`)
     })
-    .option('-t, --template', 'template for library', 'typescript')
+    .option('-t, --template', 'template for library', 'components')
 
 
 
